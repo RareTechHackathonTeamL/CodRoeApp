3bfd1180daabcbd8bf44f011564b0ff25279e4dd
robe <robe@shibauratetsuyanoMacBook-Air.local>
2025-07-24 22:31:10 +0900

    0724_01

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, url_for
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from models import db, User
 import os
+import pymysql
+import uuid
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///chatapp.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://testuser:testuser@MySQL/chatapp'
 app.config['SECRET_KEY'] = os.urandom(24)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -16,9 +18,9 @@ db.init_app(app)
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def top():
-    return render_template('login.html')
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -41,22 +43,28 @@ def logout():
     flash('ログアウトしました。')
     return redirect('/login')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET'])
+def register_view():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
+    #if request.method == 'POST':
+        # password = request.form.get('password')
         user = User(
+            user_id = uuid.uuid4(),
             user_name = request.form.get('user_name'),
             email = request.form.get('email'),
-            password = request.form.get('password'),
-            passwordConfirmation = request.form.get('password-confirmation'),
+            password_hash = request.form.get('password'),
+            # passwordConfirmation = request.form.get('password-confirmation'),
         )
         db.session.add(user)
         db.session.commit()
         flash('ユーザを登録しました。')
         return redirect('/login')
-    else:
-        return redirect(url_for('register'))
+    #else:
+    #    return render_template('register.html')
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     user_name = db.Column(db.String(255),  nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    icon_img = db.Column(db.String(255))
+    # icon_img = db.Column(db.String(255))
     # company_id = db.Column(db.Integer, db.schema.ForeignKey("companies.id", name="?????", nullable=False))
     # nickname = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, nullable=False)

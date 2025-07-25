bc6a637b5b1c6600770b8b2fb4cef5b24792a47e
robe <robe@shibauratetsuyanoMacBook-Air.local>
2025-07-25 23:58:59 +0900

    0725-01

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -5,6 +5,7 @@ from models import db, User
 import os
 import pymysql
 import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://testuser:testuser@MySQL/chatapp'
@@ -20,53 +21,78 @@ def load_user(user_id):
 
 @app.route('/', methods=['GET'])
 def top():
-    return render_template('register.html')
+    return render_template('login.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == "POST":
-        email = request.form.get('email')
-        password = request.form.get('password')
-        # Userテーブルからemailに一致するユーザを取得
-        user = User.query.filter_by(email = email).first()
-        if db.check_password_hash(user.password, password):
-            login_user(user)
-            flash('ログインに成功しました。')
-            return redirect('/chat')
+@app.route('/login', methods=['GET'])
+def login_view():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    # Userテーブルからemailに一致するユーザを取得
+    user = User.query.filter_by(email=email).first()
+    if user == None:
+        flash('Eメールアドレスまたはパスワードが間違っています。')
+        return render_template('login.html')
     else:
-        return render_template('home.html')
+        if check_password_hash(user.password, password):
+            login_user(user)
+            user_name = user.user_name
+            flash( user_name + 'さん！おかえりなさい！')
+            return render_template('home.html')
+        else:
+            flash('Eメールアドレスまたはパスワードが間違っています。')
+            return render_template('login.html')
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('ログアウトしました。')
-    return render_template('new.html')
+    return redirect(url_for('login_view'))
 
 @app.route('/register', methods=['GET'])
 def register_view():
     return render_template('register.html')
 
 @app.route('/register', methods=['POST'])
-def register():
-    #if request.method == 'POST':
-        # password = request.form.get('password')
+def register_process():
+    user_name = request.form.get('user_name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    passwordConfirmation = request.form.get('password-confirmation') 
+    registered_user = User.query.filter_by(email=email).first()
+    registered_name = User.query.filter_by(user_name=user_name).first()
+
+    if registered_user != None:
+        flash('既にユーザ登録されているようです。')
+    elif registered_name != None:
+        flash('同じユーザ名のユーザが存在します。別の名前で登録してください。')
+    elif user_name == '' or email =='' or password == '' or passwordConfirmation == '':
+            flash('空のフォームがあるようです。')
+    elif password != passwordConfirmation:
+        flash('入力された2つのパスワードが一致しません。')
+    else:
         user = User(
             user_id = uuid.uuid4(),
             user_name = request.form.get('user_name'),
             email = request.form.get('email'),
-            password = request.form.get('password'),
+            password = generate_password_hash(password),
             created_at = '2025-07-24'
-            # passwordConfirmation = request.form.get('password-confirmation'),
         )
         db.session.add(user)
         db.session.commit()
         login_user(user)
         db.session.close()
-        flash('ユーザを登録しました。')
+        flash( 'ようこそ！' + user_name + 'さん！ユーザを登録しました。')
         return render_template('home.html')
-    #else:
-    #    return render_template('register.html')
+    return render_template('register.html')
+
+@app.route('/chat', methods=['GET'])
+def chat_view():
+    return render_template('home.html')
     
 
 if __name__ == '__main__':
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.String(255), primary_key=True)
-    user_name = db.Column(db.String(255),  nullable=False)
+    user_name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     # icon_img = db.Column(db.String(255))
@@ -21,8 +21,8 @@ class User(UserMixin, db.Model):
     def get_id(self):
         return self.user_id  
 
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
+    #def set_password(self, password):
+    #    self.password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    #def check_password(self, password):
+    #    return check_password_hash(self.password, password)

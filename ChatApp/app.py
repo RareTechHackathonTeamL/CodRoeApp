da7c1f3a60cc8d33ce67975a34dd5e04f4b147b6
robe <robe@shibauratetsuyanoMacBook-Air.local>
2025-08-02 21:52:14 +0900

    0802_before_pull

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, request, flash, session
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from models import User, Chat, Message
 import pymysql
 import uuid
@@ -14,40 +14,43 @@ app = create_app()
 def load_user(user_id):
     return User.query.get(user_id)
 
+# ルートパスへのアクセスをログインページへリダイレクト
 @app.route('/', methods=['GET'])
 def top():
     return redirect(url_for('login_view'))
 
+# ログインページ表示
 @app.route('/login', methods=['GET'])
 def login_view():
     return render_template('login.html')
 
+# ログイン処理
 @app.route('/login', methods=['POST'])
 def login_process():
     email = request.form.get('email')
     password = request.form.get('password')
     # Userテーブルからemailに一致するユーザを取得
-    user = User.query.filter_by(email=email).first()
-    # user_id = user.user_id
-    if user == None:
-        flash('Eメールアドレスまたはパスワードが間違っています。')
-        return render_template('login.html')
+    user = User.find_by_email(email)
+
+    if email == '' or password == '':
+        flash('空のフォームがあるっタラコ！')
+    elif user == None:
+        flash('Eメールアドレスか、パスワーを間違っタラコ？')
+    elif check_password_hash(user.password, password) == False:
+        flash('Eメールアドレスか、パスワーをsが間違っタラコ？')
     else:
-        if check_password_hash(user.password, password):
-            login_user(user)
-            user_name = user.user_name
-            #user_id = user.user_id
-            flash('おかえりなさい！ ' + user_name + 'さん！')
-            return redirect(url_for('chats_view'))
-        else:
-            flash('Eメールアドレスまたはパスワードが間違っています。')
-            return render_template('login.html')
+        login_user(user)
+        user_name = user.user_name
+        flash('おかえり！ ' + user_name + 'さん！')
+        return redirect(url_for('chats_view'))
+    return render_template('login.html')
+            
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('ログアウトしました。')
+    flash('ログアウトしタラコ！')
     return redirect(url_for('login_view'))
 
 
@@ -57,34 +60,24 @@ def register_view():
 
 @app.route('/register', methods=['POST'])
 def register_process():
-    user_name = request.form.get('user_name')
-    email = request.form.get('email')
+    new_uname = request.form.get('user_name')
+    new_email = request.form.get('email')
     password = request.form.get('password')
     passwordConfirmation = request.form.get('password-confirmation') 
-    registered_user = User.query.filter_by(email=email).first()
-    registered_name = User.query.filter_by(user_name=user_name).first()
+    registered_email= User.find_by_email(new_email)
+    registered_name = User.find_by_uname(new_uname)
 
-    if registered_user != None:
-        flash('このEメールアドレスは既に使用されています。別のEメールアドレスで登録してください。')
-    elif registered_name != None:
-        flash('このユーザは既に使用されています。別のユーザ名で登録してください。')
-    elif user_name == '' or email =='' or password == '' or passwordConfirmation == '':
-        flash('空のフォームがあるようです。')
+    if new_uname == '' or new_email =='' or password == '' or passwordConfirmation == '':
+        flash('空のフォームがあるっタラコ！')
     elif password != passwordConfirmation:
-        flash('パスワードが一致しません。')
+        flash('パスワードが一致しないっタラコ！')
+    elif registered_name != None:
+        flash('ごめんたいm(_ _)m このユーザ名は既に登録されタラコ...')  
+    elif registered_email != None:
+        flash('ごめんたいm(_ _)m このEメールアドレスは既に登録されタラコ...')
     else:
-        user = User(
-            user_id = uuid.uuid4(),
-            user_name = request.form.get('user_name'),
-            email = request.form.get('email'),
-            password = generate_password_hash(password),
-            created_at = '2025-07-24'
-        )
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        db.session.close()
-        flash( 'ようこそ！ ' + user_name + 'さん！')
+        User.regist(new_uname, new_email, password)
+        flash( 'ようこそ！ ' + new_uname + 'さん！')
         return redirect(url_for('chats_view'))
     return render_template('register.html')
     
@@ -92,52 +85,54 @@ def register_process():
 @login_required
 def chats_view():
     chats = Chat.query.all()
-    # chats = Chat.query.filter_by(Chat.user_id == current_user.get_id()).order_by(Chat.created_at).all()
     return render_template('chats.html', chats=chats)
 
+# チャット作成画面遷移
+@app.route('/chat/create', methods=['GET'])
+@login_required
+def chat_create_view():
+    return render_template('chatsCreate.html')
 
 # チャットルーム作成
-@app.route('/chat', methods=['POST'])
+@app.route('/chat/create', methods=['POST'])
+@login_required
 def create_chat():
     new_chat_name = request.form.get('chat_name')
+    if new_chat_name == '':
+        return redirect(url_for('chat_create_view'))
     chat_exist = Chat.find_by_name(new_chat_name)
 
     if chat_exist == None:
         chat_id = uuid.uuid4()
-        # TODO: ログイン機能が実装された後に修正
-        user_id = 'test'# session.get('user_id')
+        user_id = current_user.get_id()
         chat_detail = request.form.get('detail')
-        # 追加機能用
-        # chat_type = request.form.get('chat_type')
+        # TODO: 追加機能用 chat_type = request.form.get('chat_type')
         Chat.create(chat_id, user_id, new_chat_name, chat_detail)
-        # TODO: 実装によって、リダイレクト先を変更
-        return render_template('chat.html')
+        return redirect(url_for('chats_view'))
     else:
         error = 'すでに同じ名前のチャンネルが存在しています'
-        # TODO: エラー時にどう対応するかによって変更
-        return error
+        return render_template('chatsCreate.html', error=error)
 
 # チャットへ遷移
 @app.route('/chat/<chat_id>/messages', methods=['GET'])
-def open_chat(chat_id):
+@login_required
+def messages_view(chat_id):
     chat_room = Chat.find_by_chat_info(chat_id)
     messages = Message.get_messages(chat_id)
 
-    # TODO:user_idも返す
     return render_template('messages.html', chat=chat_room, messages=messages)
 
 # メッセージ作成
 @app.route('/chat/<chat_id>/messages', methods=['POST'])
+@login_required
 def create_message(chat_id):
-    # メッセージ受け取り
     message = request.form.get('message')
     id = uuid.uuid4()
-    user_id = 'test'
-    # データベース登録
+    user_id = current_user.get_id() # TODO:session.get('id')では行かない理由を調べる・聞く
+    # TODO: 追加機能・メッセージではなくスタンプの場合用
     if message:
         Message.create(id, user_id, chat_id, message)
-    # 終了
-    return 'OK'
+    return redirect(f'/chat/{chat_id}/messages')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, String, Integer, DateTime
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
+import uuid
 from __init__ import db
 
-
+# Userテーブル
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.String(255), primary_key=True)
@@ -17,9 +18,58 @@ class User(UserMixin, db.Model):
     created_at = db.Column(db.DateTime, nullable=False)
     update_at = db.Column(db.DateTime)  # 更新日時
 
-    def get_id(self):
-        return self.user_id  
+    chat = db.relationship('Chat', backref='users')
+    messages = db.relationship('Message', backref='users')
 
+    # ユーザーIDの取得
+    def get_id(self):
+        return self.user_id
+
+    # ユーザ登録
+    @classmethod
+    def regist(cls, user_name, email, password):
+        user_id=uuid.uuid4()
+        now = datetime.datetime.now()
+        password = generate_password_hash(password)
+        new_user = User(user_id=user_id, user_name=user_name, email=email, password=password, created_at=now)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            print(e)
+        finally:
+            login_user(new_user)
+            db.session.close()
+    
+    # 登録済みEメールアドレスの確認
+    @classmethod
+    def find_by_email(cls, reserch_email):
+        try:
+            result = db.session.query(User).filter(User.email == reserch_email).first()
+            return result
+        except Exception as e:
+            print(e)
+        finally:
+            db.session.close()
+
+    # 登録済みユーザ名の確認
+    @classmethod
+    def find_by_uname(cls, reserch_name):
+        try:
+            result = db.session.query(User).filter(User.user_name == reserch_name).first()
+            return result
+        except Exception as e:
+            print(e)
+        finally:
+            db.session.close()
+
+
+
+
+
+
+    
 
 
 # Chatテーブル
@@ -27,7 +77,7 @@ class Chat(db.Model):
     __tablename__ = 'chat'
 
     id = db.Column(db.String(255), nullable=False, primary_key=True)
-    # user_id = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.String(255), db.ForeignKey('users.user_id'), nullable=False)
     chat_name = db.Column(db.String(255), nullable=False)
     chat_type = db.Column(db.Integer, nullable=False)
     detail = db.Column(db.String(255), nullable=True)
@@ -39,13 +89,12 @@ class Chat(db.Model):
     @classmethod
     def create(cls, chat_id, user_id, chat_name, detail):
         now = datetime.datetime.now()
-        chat_new = Chat(id=chat_id, chat_name=chat_name, chat_type=1, detail=detail, created_at=now, update_at=now)
+        chat_new = Chat(id=chat_id, user_id=user_id, chat_name=chat_name, chat_type=1, detail=detail, created_at=now, update_at=now)
         try:
-            with db.session.begin():
-                db.session.add(chat_new)
-                db.session.commit()
+            db.session.add(chat_new)
+            db.session.commit()
         except Exception as e:
-            db.session.rolllback()
+            db.session.rollback()
             print(e)
         finally:
             db.session.close()
@@ -53,8 +102,7 @@ class Chat(db.Model):
     @classmethod
     def find_by_name(cls, reserch_chat_name):
         try:
-            with db.session.begin():
-                result = db.session.query(Chat).filter(Chat.chat_name == reserch_chat_name).first()
+            result = db.session.query(Chat).filter(Chat.chat_name == reserch_chat_name).first()
             return result
         except Exception as e:
             print(e)
@@ -64,9 +112,8 @@ class Chat(db.Model):
     @classmethod
     def find_by_chat_info(cls, reserch_chat_id):
         try:
-            with db.session.begin():
-                result = db.session.query(Chat).filter(Chat.id == reserch_chat_id).first()
-            return {"chat_name": result.chat_name, "detail": result.detail}
+            result = db.session.query(Chat).filter(Chat.id == reserch_chat_id).first()
+            return {"id": result.id, "chat_name": result.chat_name, "detail": result.detail}
         except Exception as e:
             print(e)
         finally:
@@ -78,7 +125,7 @@ class Message(db.Model):
     __tablename__ = 'messages'
 
     id = db.Column(db.String(255), nullable=False, primary_key=True)
-    # user_id = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.String(255), db.ForeignKey('users.user_id'), nullable=False)
     chat_id = db.Column(db.String(255), db.ForeignKey('chat.id'), nullable=False)
     message = db.Column(db.Text)
     # stamp_id = db.Column(db.String(255))
@@ -88,10 +135,9 @@ class Message(db.Model):
     @classmethod
     def create(cls, id, uid, cid, message):
         now = datetime.datetime.now()
-        insert_message = Message(id=id, chat_id=cid, message=message, created_at=now)
+        insert_message = Message(id=id, user_id=uid, chat_id=cid, message=message, created_at=now)
         try:
-            with db.session.begin():
-                db.session.add(insert_message)
+            db.session.add(insert_message)
             db.session.commit()
         except Exception as e:
             print(e)
@@ -101,9 +147,7 @@ class Message(db.Model):
     @classmethod
     def get_messages(cls, cid):
         try:
-            with db.session.begin():
-                messages = db.session.query(Message).filter(Message.chat_id == cid).all()
-            
+            messages = db.session.query(Message).filter(Message.chat_id == cid).all()
             result = [{
                 'message': m.message,
                 'created_at': m.created_at,
@@ -112,4 +156,4 @@ class Message(db.Model):
         except Exception as e:
             print(e)
         finally:
-            db.session.close()
+            db.session.close()  # TODO: flask_sqlalchemyでは必要ないというものを見た（公式ドキュメントには書いていない）必要か聞く
